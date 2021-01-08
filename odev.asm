38623755fa40896dd4add08c85465c08f0152cbd
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2021-01-08 21:47:37 +0300

    Add files via upload

--- odev.asm
+++ odev.asm
@@ -1,46 +1,41 @@
-; A sayisi=1019     B sayisi=19AB
-    LIST    P=16F877A
-    INCLUDE "P16F877A.INC"
-     CBLOCK  H'20'
-    aa,bu,BA,BU
-    ENDC
-    BSF     STATUS,5
-    MOVLW   H'03'
-    MOVWF   TRISA
-    CLRF    TRISB
-    MOVLW   H'06'
-    MOVWF   ADCON1
-    BCF     STATUS,5
-    CLRF    PORTB
-TEST_ALT
-    MOVLW   H'65'
-    MOVWF   aa      
-    MOVLW   H'AB'
-    MOVWF   BA      
-    MOVLW   H'62'
-    MOVWF   bu      
-    MOVLW   H'19'
-    MOVWF   bu      
-    MOVLW   H'62'
-    MOVWF   bu  
-    MOVF    aa,W
-    ADDWF   BA,F
-    BTFSC   STATUS,C    
-    INCF    bu,F        
-    BTFSC   PORTA,0
-    GOTO    $-1
-    BTFSS   PORTA,0
-    GOTO    $-1
-    MOVF    BA,W
-    MOVWF   PORTB
-TEST_UST
-    MOVF    bu,W        
-    ADDWF   BU,F
-    BTFSC   PORTA,1
-    GOTO    $-1
-    BTFSS   PORTA,1
-    GOTO    $-1
-    MOVF    bu,W
-    MOVWF   PORTB
-    GOTO    TEST_ALT    
-    END
+list P=16F628A
+include "P16F628A.INC"
+cblock 20h
+AL,AH,BL,BH
+	ENDC
+	ORG h'00'
+	CLRF PORTB
+	BANKSEL TRISB
+	CLRF TRISB
+	MOVLW H'FF'
+	MOVWF TRISA
+	BANKSEL PORTB
+	MOVLW h'07'
+	MOVWF CMCON
+BASLA
+	MOVLW h'04'
+	MOVWF AL
+	MOVLW h'00'
+	MOVWF AH
+	MOVLW h'01'
+	MOVWF BL
+	MOVLW h'00'
+	MOVWF BH
+CIKAR
+	MOVF BL,W
+	SUBWF AL,F
+	BTFSS STATUS,0
+	DECF AH,F
+	MOVF BH,W
+	SUBWF AH,F
+ALT_BAYT_GOSTER
+	MOVF AL,W
+	MOVWF PORTB
+TEST_RAO
+	BTFSC PORTA,0
+	GOTO TEST_RA0
+	MOVF AH,W
+	MOVWF PORTB
+DONGU
+	GOTO DONGU
+END
