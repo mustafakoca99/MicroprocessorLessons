7bd3a2c31a2383286e23dfe8497e4db2a0337189
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2021-01-22 17:50:31 +0300

    Add files via upload

--- odev.asm
+++ odev.asm
@@ -1,41 +1,78 @@
-list P=16F628A
-include "P16F628A.INC"
-cblock 20h
-AL,AH,BL,BH
-	ENDC
-	ORG h'00'
-	CLRF PORTB
-	BANKSEL TRISB
-	CLRF TRISB
-	MOVLW H'FF'
-	MOVWF TRISA
-	BANKSEL PORTB
-	MOVLW h'07'
-	MOVWF CMCON
-BASLA
-	MOVLW h'04'
-	MOVWF AL
-	MOVLW h'00'
-	MOVWF AH
-	MOVLW h'01'
-	MOVWF BL
-	MOVLW h'00'
-	MOVWF BH
-CIKAR
-	MOVF BL,W
-	SUBWF AL,F
-	BTFSS STATUS,0
-	DECF AH,F
-	MOVF BH,W
-	SUBWF AH,F
-ALT_BAYT_GOSTER
-	MOVF AL,W
-	MOVWF PORTB
-TEST_RAO
-	BTFSC PORTA,0
-	GOTO TEST_RA0
-	MOVF AH,W
-	MOVWF PORTB
-DONGU
-	GOTO DONGU
+﻿LIST P=16F628A
+
+__CONFIG h'3C30'
+
+abc equ 0x0C
+abcd equ 0x0D
+abcde equ 0x0E
+abcdef equ 0x0f	
+ 		
+	
+	clrf portb
+	bsf status,5
+	clrf portb
+	movlw d'031'
+	movwf porta
+	bcf status,5
+makuuu	
+	btfsc porta,0
+	goto makuuu	
+gubyo
+	movf xd ,w
+	call dongu1
+	movlw portb
+	call dongu2	
+	incf abcd,f
+	decfsz abc,f
+	goto mustafa
+	goto makuuu	
+kesme 
+	bcf intcon,inte 
+	movlw b'00000100'
+	xorwf portb,f	
+	bcf intcon,intf
+	bsf intcon,inte
+tamdondurme
+	movlw d'004'
+	movlw abc
+	clrf abcd
+mustafa
+	movf abcd,w
+	call adımtab
+	movwf h'06'
+	call gecikme2
+	incf abcd,f
+	decfsz abc,f
+	goto adımtab
+	goto tamdondurme
+adımtab
+	addwf h'02',f
+	retlw d'003'
+	retlw d'006'
+	retlw d'009'
+	retlw d'012'
+	retfie
+adımtablosu
+	addwf h'02',f
+	retlw d'001'
+	retlw d'003'
+	retlw d'004'
+	retlw d'012'
+	retlw d'008'
+	retlw d'024'
+	retlw d'016'
+	
+dongu1
+ 	movlw d'000'
+	movwf abcde
+dongu2
+	movlw d'000'
+	movwf abcdef
+bit
+	decfsz abcdef,f
+	goto bit
+	decfsz abcde,f
+	goto dongu2
+	return 
 END
+	
